78e23f2e5224f758e2a3b8e78012319416f14074
shirleydhaka <140597518+shirleydhaka@users.noreply.github.com>
2024-04-18 10:08:33 +0530

    Update Enter.js

--- Enter.js
+++ Enter.js
@@ -76,32 +76,34 @@ const mic_btn = document.querySelector('#mic');
               chunks.push(e.data);
           };
       
-          recorder.onstop = e => {
-              const blob = new Blob(chunks, { type: "audio/wav; codecs=opus" });
-              chunks = [];
-      
-              const audioURL = window.URL.createObjectURL(blob);
-              playback.src = audioURL;
-              const rollNumber = document.getElementById("rollNumber").value;
-              const radioButtonValue = document.querySelector('input[name="namePresentYes"]:checked').value;
-              const filename = rollNumber + radioButtonValue + '.wav';
+         recorder.onstop = e => {
+           const blob = new Blob(chunks, { type: "audio/wav; codecs=pcm" }); // Changed codecs
+           chunks = [];
 
-              const saveFile = () => {
-                  const a = document.createElement('a');
-                  const url = window.URL.createObjectURL(blob);
-                  a.href = url;
-                  a.download = filename; 
-                  document.body.appendChild(a);
-                  a.click();
-                  setTimeout(() => {
-                      document.body.removeChild(a);
-                      window.URL.revokeObjectURL(url);
-                  }, 0);
-              };
-      
-              saveFile();
-          };
-      
+           const audioURL = window.URL.createObjectURL(blob);
+           playback.src = audioURL;
+           const rollNumber = document.getElementById("rollNumber").value;
+           const radioButtonValue = document.querySelector('input[name="namePresentYes"]:checked').value;
+           const filename = rollNumber + radioButtonValue + '.wav';
+
+           const saveFile = () => {
+             
+             const a = document.createElement('a');
+             const url = window.URL.createObjectURL(blob);
+             a.href = url;
+             a.download = filename; 
+             document.body.appendChild(a);
+             a.click();
+             setTimeout(() => {
+               document.body.removeChild(a);
+               window.URL.revokeObjectURL(url);
+             }, 0);
+           };
+
+           saveFile();
+         };
+
+          
           can_record = true;
         }
 
